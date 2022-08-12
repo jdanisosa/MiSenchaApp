e5757ffcda3875602f27663d8244435225a1a5cd
lkdubonc <102050221+lkdubonc@users.noreply.github.com>
2022-08-12 08:19:37 -0600

    Quitando datecolumn

--- classic/src/view/empresa/Empresa.js
+++ classic/src/view/empresa/Empresa.js
@@ -1,4 +1,4 @@
-Ext.define('MiAppSencha.view.empresa.Empresa',{
+Ext.define('MiAppSencha.view.empresa.Empresa', {
     extend: 'Ext.Panel',
     layout: 'border',
     xtype: 'empresa',
@@ -9,13 +9,13 @@ Ext.define('MiAppSencha.view.empresa.Empresa',{
             layout: 'fit',
             xtype: 'grid',
             title: 'Empresas',
-            store: { type: 'empresas'},
+            store: { type: 'empresas' },
             columns: [
-                {text: 'Id', dataIndex: 'id', align: 'center', flex: 1},
-                {text: 'Nombre', dataIndex: 'nombre', align: 'center', flex: 1},
-                { text: 'Fecha de Inicio', dataIndex: 'fechaInicio', xtype: 'datecolumn', format: 'd-m-Y', flex: 1, align: 'center'},
-                {text: 'Hora de Inicio', dataIndex: 'horaInicio', align: 'center', flex: 1},
-                {text: 'Direccion', dataIndex: 'direccion', align: 'center', flex: 1},
+                { text: 'Id', dataIndex: 'id', align: 'center', flex: 1 },
+                { text: 'Nombre', dataIndex: 'nombre', align: 'center', flex: 1 },
+                { text: 'Fecha de Inicio', dataIndex: 'fechaInicio', flex: 1, align: 'center' },
+                { text: 'Hora de Inicio', dataIndex: 'horaInicio', align: 'center', flex: 1 },
+                { text: 'Direccion', dataIndex: 'direccion', align: 'center', flex: 1 },
             ]
         }
     ]
